7288f86c7fdcc5525b2483c6a7aa846db7de8660
Alvi <134646812+AlviBOT@users.noreply.github.com>
2023-05-25 22:44:46 +0300

    Add files via upload

---
+++ Alvi Deprem Bilgi/src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  token: "",
+  mongo: "",
+  davet: "https://discord.com/api/oauth2/authorize?client_id=1031644654443188304&permissions=8&scope=bot%20applications.commands",
+  destek: "https://discord.gg/YR6tZKVy"
+}
